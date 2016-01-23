07131e1a2c0806babc68beef4f9c84bace64babf
Vanderson Mota <vanderson.mota@gmail.com>
2016-01-23 10:43:12 -0200

    version bump

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='t_dict',
-    version='0.1.0',
+    version='0.1.1',
     description='Traversing and Querying Dicts the easy way',
     long_description=readme + '\n\n' + history,
     author='Vanderson Mota',
